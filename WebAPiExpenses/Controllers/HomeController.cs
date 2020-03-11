1c393191bc2ceaa1e86167ccffe43d182860f88a
adrianozovka <adrianozovka@gmail.com>
2020-03-11 09:45:12 -0300

    Ajustando readme and comments

--- WebAPiExpenses/Controllers/HomeController.cs
+++ WebAPiExpenses/Controllers/HomeController.cs
@@ -45,29 +45,7 @@ namespace WebAPiExpenses.Controllers
                     token = token
                 };
             
-        }
-
-        /*
-        [HttpGet]
-        [Route("anonymous")]
-        [AllowAnonymous]
-        public string Anonymous() => "Anônimo";
-
-        [HttpGet]
-        [Route("authenticated")]
-        [Authorize]
-        public string Authenticated() => String.Format("Autenticado - {0}", User.Identity.Name);
-
-        [HttpGet]
-        [Route("employee")]
-        [Authorize(Roles = "employee,manager")]
-        public string Employee() => "Funcionário";
-
-        [HttpGet]
-        [Route("manager")]
-        [Authorize(Roles = "manager")]
-        public string Manager() => "Gerente";
-        */
+        }     
 
     }
 }
